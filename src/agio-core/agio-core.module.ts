8b65b8310b9f88904e0f42b3ec6a53a6d5f811d2
Naitou25 <juanperezp25@gmail.com>
2018-05-09 10:02:40 +0200

    Modificaciones2

---
+++ src/agio-core/agio-core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule, Optional, SkipSelf, } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [],
+  exports: [],
+})
+export class AgioCoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: AgioCoreModule) {
+    if (parentModule) {
+      const msg = `ModuleName has already been loaded.
+        Import ModuleName once, only, in the root AppModule.`;
+      throw new Error(msg);
+    }
+  }
+ }
